691ed712f3534e4a41e57fabe5a03b22bd25e1ee
d4vves <d4vves@gmail.com>
2020-07-31 14:03:53 -0500

    artifact delete route

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -97,7 +97,7 @@ const Profile = (props) => {
 
   const removeFromProfile = (e) => {
     e.preventDefault()
-    axios.delete(`${process.env.REACT_APP_SERVER_URL}profile/artifact/${e.target.id}`)
+    axios.delete(`${process.env.REACT_APP_SERVER_URL}profile/artifact/${artifact._id}`)
     handleClose()
   }
 
